beaafcb58142419239c5bf96af5e0d6c659dadc6
xxxmoney <xxxmoney111@gmail.com>
2025-01-05 17:00:32 +0100

    Prettier and eslint, formatting, other

---
+++ tauri-app/src/common/apiServices/sample.api.ts
@@ -0,0 +1,7 @@
+import { api } from '@/axios';
+
+export const SampleApi = {
+  get() {
+    return api;
+  }
+};
--- tauri-app/vite.config.ts
+++ tauri-app/vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+//import {path} from "@tauri-apps/api";
+import path from 'path';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -7,6 +9,12 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -19,14 +27,14 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
+          protocol: 'ws',
           host,
-          port: 1421,
+          port: 1421
         }
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
+      ignored: ['**/src-tauri/**']
+    }
+  }
 }));
