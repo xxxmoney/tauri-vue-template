ab87b42d419cffa2cedb231b43a39136947d12a3
xxxmoney <xxxmoney111@gmail.com>
2025-01-05 17:38:52 +0100

    Primevue, tailwind and other setup

--- tauri-app/src/main.ts
+++ tauri-app/src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import '@/assets/style.css';
+import { router } from '@/router';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+const pinia = createPinia();
+app.use(pinia);
+
+app.use(PrimeVue, {
+  theme: 'none'
+});
+app.use(router);
+
+app.mount('#app');
